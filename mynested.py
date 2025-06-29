cc275218ad50e6c669fe172078226e1c59140cb3
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-06-29 15:12:32 +0530

    Add files via upload

---
+++ mynested.py
@@ -0,0 +1,13 @@
+num = 20
+if(num<0):
+    print("Negative")
+elif(num>0):
+    if(num<=10):
+        print("number is between 1-10")
+    elif(num>10 and num<= 21):
+        print("number is between 11-20")
+    else:
+        print("print number is greater than 20")
+else:
+    print("number is zero ")                
+
