887d18d58bdeed8e643afad7f4daa9d5571091a5
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-06-29 14:05:22 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,13 @@
+#Task on string methods
+str = "Coding is fun, and coding is creative."
+print(str. count("coding"))
+str1 = "I love c++, c++ if fun"
+print(str1. replace("c++" , "Python"))
+str2 = "i love python"
+print(str2. title())
+str3 = "user@example.com"
+print(str3. endswith("@gmail.com"))
+str4 = "Python is great"
+print(str4. index('great'))
+str5 = "Hello World"
+print(str5. count("H"))
