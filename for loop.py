ccb8459b800e79b8d1efbaf27637ee2006d12499
PraeyaSaraswat <pragyasaraswat1306@gmail.com>
2025-07-01 14:47:44 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,29 @@
+colors = ["red" , "green" , "blue" , "yellow"]
+for x in colors:
+    print(x)
+    for i in x:
+        print(i)
+
+#range
+for i in range(5):
+    print(i)
+else:
+    print("loop end")
+#for i in range(20000):
+   # print(i , "How are you")    
+for i in range(1,12,3):
+    print(i)
+
+
+for i in range(5):
+    for j in range(i+1):
+        print(i,j)
+
+
+fruits = ["apple","banana" ,"grapes"]
+for fruit in fruits:
+    print(fruit)
+else:
+    print("kuch nahi")  
+    
+                 
